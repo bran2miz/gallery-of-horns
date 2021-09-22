2a02f4237e413c900fc787d7f64ceeed0755b4c7
brandonmizutani <bran2miz@gmail.com>
2021-09-22 12:04:36 -0700

    added imports and tags within the equipment and main js files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-
+import Header from './Components/Header.js';
+import Main from './Components/Main.js';
+import Footer from './Components/Footer.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-        </p>
-        <a>
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
   );
 }
 
