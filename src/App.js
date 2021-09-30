fb7df387031a0ab5099262502e954c6f503f37b2
brandonmizutani <bran2miz@gmail.com>
2021-09-29 18:52:51 -0700

    added a form component that allows filtering of the data by horns as well as stylilzed my galaxy of horns via CSS and bootstrap

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Container from 'react-bootstrap/Container';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import SelectedBeast from './Components/SelectedBeast.js';
+import HornedForm from './Components/HornedForm.js';
 
 class App extends React.Component{
   constructor (props) {
@@ -19,21 +20,33 @@ class App extends React.Component{
   }
 
   toggleModal = () => {
-    (this.state.showModal) ? this.setState({showModal: false}): this.setState({showModal: true});
+    (this.state.showModal) ? this.setState({showModal: false}): this.setState({showModal: true });
   }
 
 selectTheBeast = (beastTitle) => {
   console.log(`selectTheBeast: ${beastTitle}`);
   let aBeast = this.state.beastData.find(item => item.title === beastTitle);
-  this.setState({selectedBeast: aBeast});
+  this.setState({ selectedBeast: aBeast });
   this.toggleModal();
 }
 
+filteredHorn = (valueOfHorns) => {
+  if(valueOfHorns === 'default') {
+    this.setState({beastData: data});
+  } else {
+    let filteredHorn = data.filter(value => value.horns === parseInt(valueOfHorns));
+    this.setState({beastData: filteredHorn});
+  }
+}
 render(){
   return (
     <>
-      <Container>
-        <Header title="Gallery of Horns"/>
+      <Container className="gallery-horns">
+        <Header title="✨Galaxy of Horns✨"/>
+        <HornedForm
+          filteredHorn ={this.filteredHorn}
+          numHorns={this.numHorns}
+        />
         <Main data={this.state.beastData} selectedTheBeast={this.selectTheBeast} />
         <Footer author="Brandon Mizutani"/>
       </Container>
